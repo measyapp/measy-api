4cf2303433b4193cf881ac941c9c278333b8be3f
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-01-23 14:51:24 -0400

    Alt. Heroku Cors

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -21,6 +21,9 @@ function verifyJWT(req, res, next){
 }
 
 function verifyHeader(req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     if ('OPTIONS' == req.method) {
         return res.sendStatus(200);
     } else {
