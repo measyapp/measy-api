e539608d63a9ef5515cd4c9e07f726686e96bc42
Marcelly Garcia <mgmm@icomp.ufam.edu.br>
2023-02-10 19:08:43 -0400

    Alt. Headers permitidas

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
             methods: ['POST', 'PUT', 'GET','OPTIONS', 'HEAD','PATCH','DELETE'],
             credentials:true,
             optionsSuccessStatus: 200,
-            allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With'] 
+            allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With','access-control-allow-origin'] 
             
                             
         }));
@@ -40,7 +40,7 @@ app.use(express.json())
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Content-Length,X-Requested-With');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,Content-Length,X-Requested-With,access-control-allow-origin');
     res.setHeader('Access-Control-Allow-Credentials', true);
     // handle OPTIONS method
     if ('OPTIONS' == req.method) {
